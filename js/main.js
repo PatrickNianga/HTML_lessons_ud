69ab7f26a2713925c2b23ce6b4a2f3b10ba929b7
PatNianga <pat.nainga@mail.ru>
2025-02-16 20:19:17 +0300

    frist section finished

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ const banner = document.querySelector(".banner__image");
 banner.addEventListener('click', ()=>{
     document.querySelector('.banner').remove();
 })
+
+
+const playVideoBtn = document.querySelector('.content__btn2')
+const video = document.querySelector('.content__clip')
+function openVideo() {
+    video.play()
+}
+playVideoBtn.addEventListener('click', ()=>{
+    openVideo()
+});
